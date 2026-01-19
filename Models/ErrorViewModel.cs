756da9be2359d686a6c43b32ceb4afd9cadf1082
Guram Gognidze Work <quram21@gmail.com>
2026-01-19 10:36:33 +0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace business_card.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
